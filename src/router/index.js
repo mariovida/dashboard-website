97b9d013886b8001d69595aebd91d31479f34dc0
Mario Vidović <60817192+mariovida@users.noreply.github.com>
2022-01-17 21:12:46 +0100

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,70 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Register from '../views/Register.vue'
+import Login from '../views/Login.vue'
+import UserInfo from '../views/AddData.vue'
+import About from '../views/About.vue'
+import Contact from '../views/Contact.vue'
+import Dashboard from '../views/Dashboard.vue'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/userinfo',
+    name: 'UserInfo',
+    component: UserInfo
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact,
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const isAuthenticated = firebase.auth().currentUser;
+  if(requiresAuth && !isAuthenticated) {
+    next("/login");
+  } else {
+    next();
+  }
+})
+
+export default router
