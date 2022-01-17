9d81549bd7992e80872a2579094691f023f6e8da
Mario Vidović <60817192+mariovida@users.noreply.github.com>
2022-01-17 21:13:18 +0100

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="home">
+    <h1 class="block-effect" style="--td: 1.2s">
+      <div class="block-reveal title" style="--bc: #4040bf; --d: .1s">Make a move</div>
+      <div class="block-reveal desc" style="--bc: #bf4060; --d: .5s">Join our growing community</div>
+    </h1>
+    <div class="reg">
+      <router-link to="/login" target="self"
+        ><button id="login">LOG IN</button></router-link
+      >
+      <router-link to="/register" target="self"
+        ><button id="signup">SIGN UP</button></router-link
+      >
+    </div>
+    <img src="../assets/startup.svg" class="startup_image"/>
+
+    <div class="goals">
+      <h1>Our goals</h1>
+      <div class="timeline">
+        <div class="goal-left">
+          <div class="content">
+            <h2>Make a budget and live by it</h2>
+            <p>
+              Budgets are focused on debts and expenses and nobody got rich by
+              focusing on their debts. You get wealthy by focusing on your
+              assets and your income. Most experts agree that budgets are
+              useful, if only to clearly define the amount of income and fixed
+              expenses in someone’s household. Creating a budget is a great
+              way to know your financial limits.
+            </p>
+          </div>
+        </div>
+        <div class="goal-right">
+          <div class="content">
+            <h2>Saving an emergency fund</h2>
+            <p>
+              In a fragile job market, emergency funds are essential.
+              Emergency funds are commonly used to fix a surprise car repair,
+              pay for a hospital stay, the mortgage, and a variety of other
+              unforeseen issues.
+            </p>
+          </div>
+        </div>
+        <div class="goal-left">
+          <div class="content">
+            <h2>Prioritize expenses and goals</h2>
+            <p>
+              Understand the difference between needs and wants, then focus on
+              the essentials first. That doesn’t mean other expenses aren’t
+              important, though. Your financial goals, such as paying off debt
+              or saving for retirement, should still receive attention. The
+              purpose of a budget, is to understand whether or not your money
+              is going toward things that you’re happy with, you’re proud of
+              and align with your values.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="goal-right">
+        <div class="content">
+          <h2>Leave room for surprises</h2>
+          <p>
+            Don’t expect your budget to be perfect. Surprises will happen, and
+            some expenses may slip through the cracks — like the occasional
+            impulse buy. But you can take precautions to soften the blow.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+@import "../style.css";
+</style>
