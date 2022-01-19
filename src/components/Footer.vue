ba223aed83c8ad84da974273c843d5fd1f75c9fe
Mario Vidović <60817192+mariovida@users.noreply.github.com>
2022-01-19 13:50:27 +0100

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="footer">
+    <footer>
+      <h3>© 2022 Dashboard Website Template | Design by <a href="https://github.com/mariovida">MV</a></h3>
+    </footer>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+footer {
+  width: 100%;
+  background-color: #333;
+  margin-top: -35px;
+  padding: 30px 0;
+  font-size: 13px;
+  text-align: center;
+  color: white;
+}
+footer h3 {
+  font-weight: 400;
+}
+footer a {
+  color: white;
+  font-weight: 600;
+  text-decoration: none;
+}
+footer a:hover {
+  color: #CCC;
+}
+</style>
